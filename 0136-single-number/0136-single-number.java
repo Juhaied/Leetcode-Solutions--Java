b7bab2c77b278a531f8faa36ab79f4c270f4cb09
Juhaied <81870698+Juhaied@users.noreply.github.com>
2023-02-26 23:05:01 +0600

    Time: 13 ms (20.71%), Space: 42.4 MB (63.43%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,19 +1,22 @@
 class Solution {
-     public int singleNumber(int[] nums) {
-       int sum = 0;
-       HashSet<Integer> set = new HashSet<>();
-       for(int i = 0;i<nums.length;i++){
-           sum+=nums[i];
-           set.add(nums[i]);
-       }
-       
-       ArrayList<Integer> list = new ArrayList<>(set);
-       int total = 0;
-       for (int i = 0;i< list.size();i++){
-           total += (list.get(i)*2);
-       }
-       
-       return total-sum;
+      public static int singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],1);
+            }
+            else{
+                map.remove(nums[i]);
+            }
+        }
+        
+        int ans = 0;
+        
+        for(Integer i : map.keySet()){
+            ans = i;
+        }
+        
+        return ans;
     }
 
 }
