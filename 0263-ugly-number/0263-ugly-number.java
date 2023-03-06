acdf319e0f2b7dc4bf52af8c7914976bae24cdc3
Juhaied <81870698+Juhaied@users.noreply.github.com>
2023-03-06 17:22:31 +0600

    Time: 1 ms (100.00%), Space: 39.8 MB (29.46%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,24 +1,23 @@
 class Solution {
-   public boolean isUgly(int n) {
-       if(n<=0){
-           return false;
-       }
-       else{
-           while ((n % 2==0)||(n % 3==0) || (n % 5==0)){
-               if(n % 2==0){
-                   n/=2;
-               }
-               else if(n % 3==0){
-                   n/=3;
-               } else if (n % 5==0) {
-                   n/=5;
-               }
-               else {
-                   break;
-               }
-           }
-           
-           return n==1;
-       }
+  public boolean isUgly(int n) {
+       // Recursive Solution
+
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n %2==0){
+            return isUgly(n/2);
+        }
+        if(n %3==0){
+            return isUgly(n/3);
+        }
+        if ((n % 5==0)){
+            return isUgly(n/5);
+        }
+        return false;
     }
+
 }
